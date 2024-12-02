b1c4dd36244e7bc5bc87016eae59273a74c0c1f6
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-12-02 19:33:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   PREFIX: process.env.PREFIX || "MBUVITECH",
   EMAIL: process.env.EMAIL || "hakay77944@daypey.com",
   PASS: process.env.PASS || "@Juni001",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot don't share your session id",
   IMAGE: process.env.IMAGE || "https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg",
 }
